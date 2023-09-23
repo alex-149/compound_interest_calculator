c069c0c721333b11f1401afa2277af21d1102abb
Alex <114363651+alex-149@users.noreply.github.com>
2023-09-23 14:25:21 +1200

    Update compoundinterest.py
    Added rounding to 2 decimal places.

--- compoundinterest.py
+++ compoundinterest.py
@@ -1,6 +1,6 @@
 def compound(x, y, z):
     final =  x * pow((1 + y / 100), z)
-    print(final)
+    print(f"{final:.2f}")
 
 x = int(input("How much are you starting with: "))
 y = int(input("Enter a pertencage: "))
